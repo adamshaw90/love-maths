9acbae9eeb0bd91a335f9e9ad17aa7bc4ef6d398
Adam Standerwick-Shaw <adamoshaw90@gmail.com>
2024-07-30 15:30:03 +0000

    Add division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,6 +48,10 @@ function runGame(gameType) {
 
         displaySubtractQuestion(num1, num2);
 
+    } else if (gameType === "division") {
+
+        displayDivisionQuestion(num1, num2);
+
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -97,6 +101,10 @@ function calculateCorrectAnswer() {
 
       return [operand1 - operand2, "subtract"];
 
+    } else if (operator === "/") {
+
+      return [operand1 / operand2, "division"]   
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -142,4 +150,15 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+   
+    
 }
